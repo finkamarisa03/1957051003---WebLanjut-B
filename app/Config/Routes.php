9eedcf09c51f1402a37bdfd146d2bd6160b3b176
finkamarisa <ximipa5.finkamarisageananda@gmail.com>
2021-10-21 20:15:43 +0700

    sixth commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,11 +7,12 @@ $routes = Services::routes();
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
-if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
-    require SYSTEMPATH . 'Config/Routes.php';
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
+	require SYSTEMPATH . 'Config/Routes.php';
 }
 
-/*
+/**
  * --------------------------------------------------------------------
  * Router Setup
  * --------------------------------------------------------------------
@@ -32,28 +33,30 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', function (){
-	$data=['title' => "Blog - Home",];
+	$data=[
+		'title' => "Blog - Home",
+	];
 	echo view('layouts/header',$data);
 	echo view('layouts/navbar');
 	echo view('v_home');
 	echo view('layouts/footer');
 });
-
 $routes->get('/admin', 'Templating::index');
 $routes->get('/admin/posts', 'AdminPostController::index');
 $routes->get('/admin/posts/create', 'AdminPostController::create');
+$routes->post('/admin/posts/store', 'AdminPostController::store');
 $routes->get('/posts', 'PostController::index');
-
-$routes->get('/about', function(){
-    $data=['title' => "Blog - About",];
-    echo view('layouts/header', $data);
-    echo view('layouts/navbar');
-    echo view('v_about');
-    echo view('layouts/footer');
+$routes->get('/about', function (){
+	$data=[
+		'title' => "Blog - About",
+	];
+	echo view('layouts/header',$data);
+	echo view('layouts/navbar');
+	echo view('v_about');
+	echo view('layouts/footer');
 });
 $routes->get('/register', 'RegisterController::index');
-$routes->post('saveRegister', 'RegisterController::saveRegister');
-
+$routes->post('/saveRegister', 'RegisterController::saveRegister');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
@@ -67,6 +70,7 @@ $routes->post('saveRegister', 'RegisterController::saveRegister');
  * You will have access to the $routes object within that file without
  * needing to reload it.
  */
-if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php')) {
-    require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
+if (file_exists(APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php'))
+{
+	require APPPATH . 'Config/' . ENVIRONMENT . '/Routes.php';
 }
